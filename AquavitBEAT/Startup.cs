5b62ea53c3caf54a89935791894fd552ee05878f
terjesaether <terjesather@gmail.com>
2017-01-02 23:29:44 +0100

    Add project files.

---
+++ AquavitBEAT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AquavitBEAT.Startup))]
+namespace AquavitBEAT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
